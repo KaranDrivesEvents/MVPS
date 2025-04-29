eb2bd592b65d198e2b3671f8aa31bd61dfa73bc7
KaranDrivesEvents <buildeventdriven@gmail.com>
2025-04-29 17:03:48 -0400

    Added: Post processing transformation
    - Added tuple3 map function to only take three string columns and push it as an output.
    - Added some comments as learning-notes.

--- MVP2/ApacheFlink/my-flink-project/src/main/java/com/pluralsight/streaming/CarListingsMapping.java
+++ MVP2/ApacheFlink/my-flink-project/src/main/java/com/pluralsight/streaming/CarListingsMapping.java
@@ -2,6 +2,7 @@ package com.pluralsight.streaming;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.util.InvalidPropertiesFormatException;
@@ -40,6 +41,16 @@ public class CarListingsMapping {
         env.socketTextStream("localhost", 9000)
            .map(new CreateCarObjects())
            .filter(new MakePriceFilter("Ford", 20000f))
+        //    Only returning tuple of 3 important attributes  make, model, type. 
+           .map(new MapFunction<Car, Tuple3<String, String, String>>() {
+               @Override
+               public Tuple3<String, String, String> map(Car car) throws Exception {
+                   return new Tuple3<>(car.make, car.model, car.type);
+               }
+           })
+        /** Can also do return custom transformed string as an output 
+        like we're multiplying the car price with tax amount.**/
+        // .map(car -> new Car(car.make, car.model, car.type, car.price * 1.05f))
            .print();
 
         env.execute();
@@ -48,7 +59,8 @@ public class CarListingsMapping {
     public static class CreateCarObjects implements MapFunction<String, Car> {
 
         @Override
-        // Overriding MapFunction | MapFunction<T, O> | T -> Input Type, O -> Output Type, O is Output type
+        /* Overriding MapFunction | MapFunction<T, O> |
+        T -> Input Type, O -> Output Type, O is Output type */
         public Car map(String carString) throws Exception {
 
             String[] tokens = carString.split(",");
@@ -56,7 +68,12 @@ public class CarListingsMapping {
             if (tokens.length < 4) {
                 throw new InvalidPropertiesFormatException("Invalid stream input: " + carString);
             }
-
+            /**
+             * Modified Car class to enforce input validation by verifying 
+             * stream lengths, throwing an IllegalArgumentException on mismatch.
+             * Implemented data transformations for consistency: 
+             * whitespace trimming and string-to-float conversion.
+             **/
             return new Car(tokens[0].trim(),
                            tokens[1].trim(),
                            tokens[2].trim(),
